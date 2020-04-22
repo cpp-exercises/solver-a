d695cddaa4e0c912d944a0d620ae9f4b5ac43121
Erel Segal-Halevi cron <erelsgl@gmail.com>
2020-04-22 15:32:48 +0300

    update solver exercise

--- Demo.cpp
+++ Demo.cpp
@@ -2,30 +2,34 @@
 //  Demo.cpp
 //  operatorOverload
 //
-//  Created by Adam Lev-Ari on 29/12/2019.
-//  Copyright © 2019 Adam Lev-Ari. All rights reserved.
+//  First version by Adam Lev-Ari on 29/12/2019.
+//  Second version by Erel Segal-Halevi on 22/04/2020.
 //
 
-
 #include <iostream>
+#include <complex>
 #include "solver.hpp"
 
 using namespace std;
 
 int main(){
-    solver::Variable x;
-    
-    solve(2*x-4 == 10);
-    cout << x << endl;     // x = 7
+    solver::RealVariable x;
 
-    solve((x^2) == 16);
-    cout << x << endl;     // x = 4 
+    double xvalue = solver::solve(2*x-4 == 10);
+    cout << xvalue << endl;  // 7
 
-    solve((2*x^2) - 4*x + 3 == 9);
-    cout << x << endl;     
+    cout << solver::solve((x^2) == 16) << endl;   // 4 or -4
+    cout << solver::solve((x^2) == -16) << endl;  // exception: no real solution
+    cout << solver::solve((x^2) + 2*x + 4 == 20 + 6*x/2 - x) << endl;   // 4 or -4
 
-    solve((x^2) + 6*x == -9);
-    cout << x << endl;
+    solver::ComplexVariable y;
+    std::complex yvalue = solver::solve(2*y-4 == 10);
+    cout << yvalue << endl;  // 7+0i  (can be in any format)
+
+    cout << solver::solve((y^2) == 16) << endl;   // 4+0i or -4+0i
+    cout << solver::solve((y^2) == -16) << endl;  // 0+4i or 0-4i
+    cout << solver::solve((y^2) + 2*y + 4 == 20 + 6*y/2 - y) << endl;   // 4+0i or -4+0i
+    cout << solver::solve(y+5i == 2*y+3i) << endl;   // 0+2i
 
     return 0;
 }
--- TestCounter.cpp
+++ TestCounter.cpp
@@ -2,7 +2,7 @@
 #include "doctest.h"
 using namespace doctest;
 
-const int MIN_TESTS = 20;
+const int MIN_TESTS = 100;
 
 struct ReporterCounter: public ConsoleReporter {
     ReporterCounter(const ContextOptions& input_options)
